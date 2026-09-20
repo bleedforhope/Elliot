b159d12076482009d39a18e196020a3fcea7febc
bleedforhope <lordspeccandir@gmail.com>
2026-09-20 05:41:07 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,38 @@
-const CACHE = "elliot-shell-v1";
-const ASSETS = ["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icon.svg"];
+const CACHE = "elliot-local-irc-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  const u = new URL(e.request.url);
-  if (u.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(cached => cached || fetch(e.request).then(r => {
-        const copy = r.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return r;
-      }))
-    );
-  }
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
